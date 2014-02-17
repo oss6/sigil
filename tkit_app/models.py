16161e957624536d518c1910ed93f4821abd04b4
Ossama <ossedb@gmail.com>
2014-02-17 22:14:58 +0100

    Added template files.

--- tkit_app/models.py
+++ tkit_app/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class Classes(models.Model):
-    pass
+    name = models.CharField(max_length=20)
+    school = models.CharField(max_length=20)
 
 
 class Students(models.Model):
-    pass
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
+    parent = models.CharField(max_length=30)
+    parent_email = models.EmailField()
+    photo = models.ImageField(upload_to='/', blank=True, null=True)
 
 
 class Attendance(models.Model):
