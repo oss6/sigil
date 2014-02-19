797bd7fdbd93740d3d4b2b78034ee71aeb53ff66
Ossama <ossedb@gmail.com>
2014-02-19 18:15:54 +0100

    Added auth system.

--- tkit_app/urls.py
+++ tkit_app/urls.py
@@ -1,11 +1,21 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.contrib.auth.views import login, logout
 from tkit_app import views
 
 urlpatterns = patterns('',
+    # Static pages
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^about/$', TemplateView.as_view(template_name="about.html")),
-    url(r'^login/$', TemplateView.as_view(template_name="login.html")),
+
+    # Sign up
+    url(r'^signup/$', views.register),
+    url(r'^signup-success/$', TemplateView.as_view(template_name="signup_success.html")),
+
+    # Login - Logout
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
+
     url(r'^classes/$', views.classes),
     url(r'^classes/(?P<class_name>[a-z])/students/$', views.students),
 )
--- tkit_app/views.py
+++ tkit_app/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
+from django.core.context_processors import csrf
+from django.contrib import auth
+from forms import RegistrationForm
 from models import *
 
 
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/signup-success/')
+    args = {}
+    args.update(csrf(request))
+    args['form'] = RegistrationForm()
+
+    return render(request, 'register.html', args)
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return redirect("/account/loggedin/")
+    else:
+        return redirect("/account/invalid/")
+
+
+def logout(request):
+    pass
+
+
 def classes(request):
-    return render(request, "classes.html")
+    return render_to_response("classes.html")
 
 
 def students(request, class_name):
