24e6d75577ba04f8a07771e529130cef4c30c617
Ossama <ossedb@gmail.com>
2014-04-27 23:21:20 +0200

    Starting presentation tool

--- tkit_app/urls.py
+++ tkit_app/urls.py
@@ -10,7 +10,7 @@ from tkit_app.views import MindMapView
 urlpatterns = patterns('',
     # Static pages
     url(r'^dashboard/$', TemplateView.as_view(template_name="base.html")),
-    url(r'^about/$', TemplateView.as_view(template_name="about.html")),
+    url(r'^info/$', TemplateView.as_view(template_name="info.html")),
     url(r'^documentation/dev/$', TemplateView.as_view(template_name="doc-dev.html")),
     url(r'^documentation/users/$', TemplateView.as_view(template_name="doc-users.html")),
 
