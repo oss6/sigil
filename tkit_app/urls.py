9f56da33f413c96635590a461b664d32962ec457
Ossama <ossedb@gmail.com>
2014-05-23 18:03:03 +0200

    Fixing pres feature

--- tkit_app/urls.py
+++ tkit_app/urls.py
@@ -39,6 +39,7 @@ urlpatterns = patterns('',
     url(r'^classes/remove/(?P<id_class>\d+)/$', views.remove_class),
     url(r'^classes/(?P<id_class>\d+)/report/grades-performance-chart/$', views.class_grades_performance_chart),
     url(r'^classes/(?P<id_class>\d+)/report/$', views.class_report),
+    url(r'^classes/(?P<id_class>\d+)/report/pdf/$', PDFTemplateView.as_view(template_name="pdf-convert.html")),
 
     # TODO
     url(r'^classes/(?P<id_class>\d+)/groupmaker/$', views.group_maker),
