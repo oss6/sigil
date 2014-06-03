e5b16401f517e014bfe8c30ab17d87b77b4476c3
Ossama <ossedb@gmail.com>
2014-06-03 12:58:03 +0200

    Changed db name

--- tkit/settings.py
+++ tkit/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tkit_storage',                      # Or path to database file if using sqlite3.
+        'NAME': 'sigildb.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
