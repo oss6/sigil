dfdf574d3dc7fbb2dbd0514bd22d723d86eeb8ca
Ossama <ossedb@gmail.com>
2014-06-10 12:34:54 +0200

    Fixed calendar tool

--- sigil/static/js/calendar.js
+++ sigil/static/js/calendar.js
@@ -9,35 +9,39 @@ $(document).ready(function() {
     // Creating events
     for (i = 0; i < TODOS_OBJ.length; i++) {
         obj = TODOS_OBJ[i].fields
-        events.push({ date: obj.date_exp, title: obj.title });
+        events.push({ date: obj.date_exp, title: obj.title, type: 'Task' });
     }
 
     for (i = 0; i < LESSONS_OBJ.length; i++) {
         obj = LESSONS_OBJ[i].fields
-        events.push({ date: obj.date, title: obj.title });
+        events.push({ date: obj.date, title: obj.title, type: 'Lezione' });
     }
 
     for (i = 0; i < ASSIGNMENTS_OBJ.length; i++) {
         obj = ASSIGNMENTS_OBJ[i].fields
-        events.push({ date: obj.date_end, title: obj.title });
+        events.push({ date: obj.date_end, title: obj.title, type: 'Compito' });
     }
 
-    // Calendar
-    $('#mini-clndr').clndr({
-        template: $('#mini-clndr-template').html(),
+    $('.cal1').clndr({
         events: events,
-
         clickEvents: {
             click: function(target) {
                 if(target.events.length) {
-                    var daysContainer = $('#mini-clndr').find('.days-container');
-                    daysContainer.toggleClass('show-events', true);
-                    $('#mini-clndr').find('.x-button').click( function() {
-                        daysContainer.toggleClass('show-events', false);
-                    });
+                    var evs = '<ul>';
+                    for (var i = 0; i < target.events.length; i++)
+                        evs += '<li>' + target.events[i].title + ' (' + target.events[i].type + ')</li>';
+                    evs += '</ul>';
+
+                    $("#modalDesc").html(evs);
+                    $('#modal').modal('show');
                 }
             }
         },
-        adjacentDaysChangeMonth: true
+        multiDayEvents: {
+            startDate: 'startDate',
+            endDate: 'endDate'
+        },
+        showAdjacentMonths: true,
+        adjacentDaysChangeMonth: false
     });
 });
